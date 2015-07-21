a86989fe79766d20827d20a499b91718037e1f37
Daniel Herrera <daniel.herrera@xoom.com>
2015-07-21 13:00:12 -0600

    views added and mapping number

--- app.rb
+++ app.rb
@@ -1,38 +1,26 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World"
-end
-
-get '/case1' do
-   @foo = 'erb'
-   erb :index
-end
-
-get '/case2' do
-  "Hello Case2"
-end
-
-get '/case3' do
-  "Hello Case3"
-end
-
-get '/case4' do
-  "Hello Case4"
+  erb :index
 end
 
 post '/postaction1' do
-  params['name']
-end
-
-post '/postaction2' do
-  params['name']
-end
-
-post '/postaction3' do
-  params['name']
-end
-
-post '/postaction4' do
-  params['name']
+  number = params['number']
+  partner = params['service']
+  l = number.length
+  number = number[l-1,l]
+  case number
+  when '0'
+    @msg = "Error 1"
+    erb :error
+  when '1'
+    @msg = "Error 2"
+    erb :error
+  when '2'
+    @msg = "Error 3"
+    erb :error
+  else
+    @msg = "unknow error"
+    erb :error
+  end
 end
